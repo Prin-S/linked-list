13528af8afab0e6a99c1057bb029a12be7c9fcb9
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-09-22 22:14:45 +0700

    Added more methods and comments to LinkedList.
    Created at(), pop(), contains(), find(), and toString() methods in LinkedList.
    Added comments to LinkedList methods.

--- linked-list.js
+++ linked-list.js
@@ -5,16 +5,16 @@ class LinkedList {
     }
 
     append(value) {
-        if (!this.head) {
+        if (!this.head) { // When the first node is appended,
             this.head = new Node(value);
         } else {
-            let currNode = this.head;
+            let currNode = this.head; // Start with the first node.
 
-            while (currNode.nextNode != null) {
-                currNode = currNode.nextNode;
+            while (currNode.nextNode != null) { // While nextNode of currNode still points to another node,
+                currNode = currNode.nextNode; // Move to nextNode.
             }
 
-            currNode.nextNode = new Node(value);
+            currNode.nextNode = new Node(value); // Once the last node is reached, create and append the new node to nextNode of currNode.
         }
 
         this.size++;
@@ -22,8 +22,8 @@ class LinkedList {
 
     prepend(value) {
         const oldHead = this.head;
-        this.head = new Node(value);
-        this.head.nextNode = oldHead;
+        this.head = new Node(value); // Create and prepend the new node as the new head.
+        this.head.nextNode = oldHead; // Move the old head to the next node.
         this.size++;
     }
 
@@ -32,17 +32,108 @@ class LinkedList {
     }
 
     getHead() {    
-        return new Node(this.head.value, this.head.nextNode.value);
+        return new Node(this.head.value, this.head.nextNode.value); // This is so that only value is shown for nextNode.
     }
 
     getTail() {
         let lastNode = this.head;
         
-        while (lastNode.nextNode != null) {
-            lastNode = lastNode.nextNode;
+        while (lastNode.nextNode != null) { // While nextNode of lastNode still points to another node,
+            lastNode = lastNode.nextNode; // Keep on assigning nextNode as lastNode.
         }
 
         return lastNode;
+    }
+
+    at(index) {
+        if (index >= this.size) {
+            return `Node not found at index = ${index}`;
+        } else if (index == this.size - 1) { // Return the last node.
+            return this.getTail();
+        } else { // Return other nodes.
+            let selectedNode = this.head;
+
+            for (let i = 0; i < index; i++) {
+                selectedNode = selectedNode.nextNode;
+            }
+
+            return new Node(selectedNode.value, selectedNode.nextNode.value);
+        }
+    }
+
+    pop() {
+        const lastNode = this.getTail().value; // Get value of the last node before it is removed.
+        let beforeLastNode = this.head;
+
+        try {
+            while (beforeLastNode.nextNode.nextNode != null) { // If nextNode of nextNode is not null,
+                beforeLastNode = beforeLastNode.nextNode; // Move to nextNode.
+            }
+
+            beforeLastNode.nextNode = null; // Once out of the loop, make nextNode of beforeLastNode null to cut the link to the last node.
+        } catch (err) { // When the final node is removed (nothing left in the linked list),
+            this.head = null;
+        }
+        
+        this.size--;
+
+        return `Popped "${lastNode}" from the list.`;
+    }
+
+    contains(value) {
+        let selectedNode = this.head;
+        
+        if (selectedNode.value == value) { // If the first node equals value,
+            return true;
+        }
+
+        while (selectedNode.nextNode != null) { // Loop the subsequent nodes.
+            selectedNode = selectedNode.nextNode;
+
+            if (selectedNode.value == value) { // If any of the subsequent nodes equals value,
+                return true;
+            }
+        }
+
+        return false; // If no match is found,
+    }
+
+    find(value) {
+        let counter = 0;
+        let selectedNode = this.head;
+        
+        if (selectedNode.value == value) { // If the first node equals value,
+            return counter;
+        }
+
+        while (selectedNode.nextNode != null) { // Loop the subsequent nodes.
+            counter++;
+            selectedNode = selectedNode.nextNode;
+
+            if (selectedNode.value == value) { // If any of the subsequent nodes equals value,
+                return counter;
+            }
+        }
+
+        return null; // If no match is found,
+    }
+
+    toString() {
+        let string = '';
+        let currNode = this.head;
+
+        if (currNode == null) { // If the first node is null (nothing in the linked list),
+            return 'The linked list is empty.';
+        } else {
+            string += `( ${currNode.value} ) -> `;
+        }
+
+        while (currNode.nextNode != null) { // While nextNode of currNode still points to another node,
+            currNode = currNode.nextNode; // Move to nextNode.
+            string += `( ${currNode.value} ) -> `;
+        }
+
+        return string + 'null'; // Append null at the end.
     }
 }
 
@@ -63,4 +154,20 @@ list.prepend("hamster");
 console.log(list.getSize());
 console.log(list.getHead());
 console.log(list.getTail());
+console.log(list.at(4));
+console.log(list.pop());
 console.log(list.getSize());
+console.log(list.getTail());
+console.log(list.contains("parrot"));
+console.log(list.find("cat"));
+console.log(list.toString());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.toString());
+console.log(list.pop());
+console.log(list.getSize());
+console.log(list.toString());
+
+list.append("snake");
+console.log(list.getSize());
+console.log(list.toString());
